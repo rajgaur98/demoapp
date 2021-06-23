d886b6a2afbd4534aa01d94bcee50c62bc65293d
Rajkumar gaur <52281264+rajgaur98@users.noreply.github.com>
2021-06-23 16:02:15 +0530

    Changes in app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 let express = require( 'express' ); 
 let app = express();
-let server = require( 'http' ).Server( app );
-let io = require( 'socket.io' )( server );
 let stream = require( './ws/stream' );
 let path = require( 'path' );
-// let favicon = require( 'serve-favicon' );
-
+// let server = require( 'http' ).Server( app );
 // app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
 app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
 
@@ -13,8 +10,11 @@ app.get( '/', ( req, res ) => {
     res.sendFile( __dirname + '/index.html' );
 } );
 
+let server = app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
+let io = require( 'socket.io' ).listen(server);
+// let favicon = require( 'serve-favicon' );
 
 io.of( '/stream' ).on( 'connection', stream );
 // start the server listening for requests
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+// app.listen(process.env.PORT || 3000, 
+// 	() => console.log("Server is running..."));
